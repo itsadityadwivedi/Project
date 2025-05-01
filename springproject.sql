fcb18793c7267a6eadf5875a30e506245d812802
itsadityadwivedi <studyandlearn1503@gmail.com>
2025-05-01 18:40:01 +0530

    Update springproject.sql

--- springproject.sql
+++ springproject.sql
@@ -84,7 +84,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `role`, `enabled`, `email`) VALUES
-(1, 'jay', '123', 'ROLE_USER', 1, 'gajerajay9@gmail.com'),
+(1, 'aditya', '123', 'ROLE_USER', 1, 'adityadwivedi.ag@gmail.com'),
 (2, 'admin', '123', 'ROLE_ADMIN', 1, '20ceuos042@ddu.ac.in');
 
 --
